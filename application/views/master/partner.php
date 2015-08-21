ca03e9fb0c6b4ec2ccedf0e0aac76b244c96052e
Nex Digital <nexdigital.software@gmail.com>
2015-08-22 03:17:10 +0700

    Customers

--- application/views/master/partner.php
+++ application/views/master/partner.php
@@ -46,7 +46,7 @@
 </table>
 
 <button class="btn btn-primary" onCLick="setPage('<?php echo base_url().'master/partner/add'?>')">Add Partner</button>
-<button class="btn btn-primary"><span class="glyphicon glyphicon-print" aria-hidden="true"></span> Print to CSV</button>
+
 
 <script>
 $(document).ready( function () {
