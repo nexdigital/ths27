8665f647e2106f5e601de9b2f25f309ab2ab1b5b
Nex Digital <nexdigital.software@gmail.com>
2015-11-23 15:25:07 +0700

    upadte

--- application/config/database.php
+++ application/config/database.php
@@ -48,17 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-if($_SERVER['SERVER_NAME'] == 'localhost') {
-	$db['default']['hostname'] = 'localhost';
-	$db['default']['username'] = 'root';
-	$db['default']['password'] = '';
-	$db['default']['database'] = 'manifest_system';
-} else {
-	$db['default']['hostname'] = 'localhost';
-	$db['default']['username'] = 'nexg1757_dbuser';
-	$db['default']['password'] = 'gbhXr&cnEH]q';
-	$db['default']['database'] = 'nexg1757_manifest_system';
-}
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'manifest_system';
 
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
